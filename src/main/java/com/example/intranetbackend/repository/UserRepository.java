7bd83a47e09c89da5e5b476562fc117edc1da7de
OmerGit <rafaelxdale@gmail.com>
2023-07-15 16:36:26 -0500

    update @RequestMapping to UserController and UserRestController.java

--- src/main/java/com/example/intranetbackend/repository/UserRepository.java
+++ src/main/java/com/example/intranetbackend/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.intranetbackend.repository;
 
 import com.example.intranetbackend.domain.User;
-import com.example.intranetbackend.dto.UserResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
