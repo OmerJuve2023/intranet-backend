557732a06bd288385cf8d8c436da6b1e7d84a0c6
OmerGit <rafaelxdale@gmail.com>
2023-07-15 10:45:54 -0500

    update @RequestMapping to UserController and UserRestController.java

--- src/main/java/com/example/intranetbackend/controller/UserController.java
+++ src/main/java/com/example/intranetbackend/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @CrossOrigin(origins = "*",
         maxAge = 3600)
 @Controller
-@RequestMapping(name = "intranet-unjfsc/get")
+@RequestMapping(name = "/intranet-unjfsc/get")
 public class UserController {
     private final UserService userService;
 
--- src/main/java/com/example/intranetbackend/controller/UserRestController.java
+++ src/main/java/com/example/intranetbackend/controller/UserRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*",
         maxAge = 3600)
 @RestController
-@RequestMapping(name = "intranet-unjfsc/add")
+@RequestMapping(name = "/intranet-unjfsc/add")
 public class UserRestController {
     private final UserService userService;
 
